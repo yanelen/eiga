1a3b3488b5caa0e92d136749aaec28cdc77be460
Yanelen <yanelen@yahoo.com>
2015-11-25 20:48:21 -0500

    added database

--- server.js
+++ server.js
@@ -2,31 +2,34 @@ var express     = require('express'),
     server      = express(),
     bodyParser  = require('body-parser'),
     mongoose    = require('mongoose'),
-    Schema      = mongoose.Schema;
+    Schema      = mongoose.Schema,
+    PORT        = process.env.PORT || 4321,
+    MONGOURI    = process.env.MONGOLAB_URI || "mongodb://localhost:27017",
+    dbname      = 'movies'
 
-var noteSchema  = new Schema({
-  note_value: { type: String, required: true },
+var MovieSchema  = new Schema({
+  Movie_value: { type: String, required: true },
   created: { type: Date, default: Date.now }
 });
-var Note = mongoose.model('Note', noteSchema);
+var Movie = mongoose.model('Movie', MovieSchema);
 
 server.use(express.static('./public'));
 server.use(bodyParser.json());
-server.get('/notes', function(request, response){
-  Note.find({}, function(err, notes){
-    response.json(notes);
+server.get('/Movies', function(request, response){
+  Movie.find({}, function(err, Movies){
+    response.json(Movies);
   })
 });
 
-server.post('/notes', function(request, response){
-  Note.create(request.body, function(err, data){
-    Note.find({}, function(err, notes){
-      response.json(notes);
+server.post('/Movies', function(request, response){
+  Movie.create(request.body, function(err, data){
+    Movie.find({}, function(err, Movies){
+      response.json(Movies);
     });
   });
 });
 
-mongoose.connect('mongodb://localhost:27017/notes');
-server.listen(3000, function(){
-  console.log("Server is listening");
+mongoose.connect(MONGOURI + "/" + dbname);
+server.listen(PORT, function () {
+  console.log("CONNECTED");
 });
