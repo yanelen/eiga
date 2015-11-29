0447fb7fe56904c6424f5e6e7be13c58828e94ae
Yanelen <yanelen@yahoo.com>
2015-11-29 11:35:14 -0500

    added mongoose

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
 var express     = require('express'),
     server      = express(),
+    bodyParser  = require('body-parser'),
+    mongoose    = require('mongoose'),
+    db          = 'movielist',
+    Schema      = mongoose.Schema,
     PORT        = process.env.PORT || 4321;
 
+mongoose.connect('mongodb://localhost:27017/db');
+
+var movieSchema  = new Schema({
+  title { type: String, required: true },
+  poster: { type: String, required: true },
+  year: { type: String, required: true }
+});
+var Moive = mongoose.model('Movie', movieSchema);
+
 server.use(express.static('./public'));
+server.use(bodyParser.json());
 
 server.get('/movielist', function(req, res){
-  console.log('I received a get request')
-  movie1 = {
-    title: 'Closer',
-    year: '2004',
-    poster: 'http://imdb.wemakesites.net/api/1.0/img/?url=http://ia.media-imdb.com/images/M/MV5BMTM5NDM3NDc1OF5BMl5BanBnXkFtZTcwNzE1MzcyMQ@@._V1_SX300.jpg'
-  };
-
-  movie2 = {
-    title: 'My Neighbor Totoro',
-    year: '1988',
-    poster: 'http://imdb.wemakesites.net/api/1.0/img/?url=http://ia.media-imdb.com/images/M/MV5BMjE3NzY5ODQwMV5BMl5BanBnXkFtZTcwNzY1NzcxNw@@._V1_SX300.jpg'
-  };
-
-  var movielist = [movie1, movie2];
-  res.json(movielist);
+  Movie.find({}, function(err, movielist){
+    res.json(movielist);
+  })
 });
 
 server.listen(PORT, function () {
