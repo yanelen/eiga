66c9eb13bcefc28a46ad2fd8cbcfb5149b4af9d2
yanelen <yanelen@yahoo.com>
2015-12-12 23:54:20 -0500

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -22,9 +22,17 @@ app.controller('MainController', function($scope, $http){
   }
 
   function getMovie(){
-    $http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
+    $http.get("http://www.omdbapi.com/?t=" + $scope.search + "&apikey=d616161f&tomatoes=true&plot=full")
      .success(function(data){
        $scope.movie = data;
+     });
+     getMovie2();
+  }
+
+  function getMovie2(){
+    $http.get("http://api.themoviedb.org/3/search/multi?query=" + $scope.search + "&api_key=42b9da5263e7b0d6c6b7a28de8b24940")
+     .success(function(data){
+       $scope.poster = data.results[0].poster_path;
      });
   }
 
@@ -47,7 +55,7 @@ app.controller('MainController', function($scope, $http){
   var movielist = [];
   $scope.create = function(){
     x = 0;
-    movie = { title: $scope.movie.Title, year: $scope.movie.Year, poster: $scope.movie.Poster }
+    movie = { title: $scope.movie.Title, year: $scope.movie.Year, poster: $scope.poster }
     for (i = 0; i < movielist.length; i++) {
       if ( movie.title === movielist[i].title ) {
         x = 1;
